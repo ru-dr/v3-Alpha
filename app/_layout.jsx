f64e0c7e9493dfa16181d879f7427ef5ca6ea1bb
Lazycoder26 <Rudra26_2004@outlook.com>
2024-01-25 22:38:51 +0530

    Refactor code and update dependencies

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,24 +1,18 @@
 import React from "react";
 import { Stack } from "expo-router";
 import { DarkTheme, ThemeProvider } from "@react-navigation/native";
-import { ClerkProvider } from "@clerk/clerk-expo";
-import { CLERK_PUBLISHABLE_KEY } from "@env";
 
 const _layout = () => {
-  const CLERK_KEY = CLERK_PUBLISHABLE_KEY;
   return (
     <ThemeProvider value={DarkTheme}>
-      <ClerkProvider publishableKey={CLERK_KEY}>
-        <Stack
-          screenOptions={{
-            headerShown: false,
-            statusBarAnimation: "fade",
-            animation: "fade_from_bottom",
-            autoHideHomeIndicator: true,
-          }}
-        />
-        {/* <Text style={{color: "#000"}}>{CLERK_PUBLISHABLE_KEY} OK</Text> */}
-      </ClerkProvider>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          statusBarAnimation: "fade",
+          animation: "fade_from_bottom",
+          autoHideHomeIndicator: true,
+        }}
+      />
     </ThemeProvider>
   );
 };
--- app/hooks/warmUpBrowser.jsx
+++ app/hooks/warmUpBrowser.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import * as WebBrowser from "expo-web-browser";
+// import React from "react";
+// import * as WebBrowser from "expo-web-browser";
 
-export const useWarmUpBrowser = () => {
-  React.useEffect(() => {
-    void WebBrowser.warmUpAsync();
-    return () => {
-      void WebBrowser.coolDownAsync();
-    };
-  }, []);
-};
+// export const useWarmUpBrowser = () => {
+//   React.useEffect(() => {
+//     void WebBrowser.warmUpAsync();
+//     return () => {
+//       void WebBrowser.coolDownAsync();
+//     };
+//   }, []);
+// };
