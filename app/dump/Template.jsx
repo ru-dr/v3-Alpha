038d4a2e3ab30d7ca655ca1807904c795a65681a
Lazycoder26 <Rudra26_2004@outlook.com>
2024-01-25 21:48:15 +0530

    Add Template component to app/dump directory

---
+++ app/dump/Template.jsx
@@ -0,0 +1,24 @@
+import { View, Dimensions } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import BackNav from "../components/BackNav";
+
+const page = () => {
+  // get current route path
+
+  const screenHeight = Dimensions.get("window").height;
+  return (
+    <SafeAreaView>
+      <StatusBar style="light" backgroundColor="#000" />
+      <View style={{ backgroundColor: "#000", height: screenHeight }}>
+        <BackNav path={"/"} titleName={"Profile"} />
+
+        {/* write from here */}
+        
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default page;
