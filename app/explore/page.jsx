86e6adbdba83353949c71af4788047ac58beeea4
Vedant Pandey <vedantpandey434@gmail.com>
2024-01-31 14:21:38 +0530

    Add Google Maps API key and integrate react-native-maps

--- app/explore/page.jsx
+++ app/explore/page.jsx
@@ -3,17 +3,20 @@ import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import BackNav from "../components/BackNav";
-
+import MapView from "react-native-maps";
 
 const page = () => {
   // get current route path
-  
-  const screenHeight = Dimensions.get('window').height;
+
+  const screenHeight = Dimensions.get("window").height;
   return (
-    <SafeAreaView >
-      <StatusBar style="light" backgroundColor="#000"/>
-      <View style={{backgroundColor: '#000', height: screenHeight}}>
-        <BackNav path={'/'}/>
+    <SafeAreaView>
+      <StatusBar style="light" backgroundColor="#000" />
+      <View style={{ backgroundColor: "#000", height: screenHeight }}>
+        <BackNav path={"/"} />
+        <View>
+          <MapView style={{ width: 400, height: 300 }}></MapView>
+        </View>
       </View>
     </SafeAreaView>
   );
