686cf549b0a18b5d12bf5c452bfdb82e913833e8
Vedant Pandey <vedantpandey434@gmail.com>
2024-02-01 09:25:35 +0530

    Added expo-location to fetch user's location

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Stack } from "expo-router";
 import { DarkTheme, ThemeProvider } from "@react-navigation/native";
+import * as Location from "expo-location";
+import { UserLocationContext } from "./context/UserLocationContext";
 
 const _layout = () => {
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+  
+    useEffect(() => {
+      (async () => {
+        
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          setErrorMsg('Permission to access location was denied');
+          return;
+        }
+  
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+        console.log(location)
+      })();
+    }, []);
+  
+    let text = 'Waiting..';
+    if (errorMsg) {
+      text = errorMsg;
+    } else if (location) {
+      text = JSON.stringify(location);
+    }
   return (
     <ThemeProvider value={DarkTheme}>
+      <UserLocationContext.Provider value={{location, setLocation}}>
       <Stack
         screenOptions={{
           headerShown: false,
@@ -13,6 +40,7 @@ const _layout = () => {
           autoHideHomeIndicator: true,
         }}
       />
+      </UserLocationContext.Provider>
     </ThemeProvider>
   );
 };
--- app/explore/page.jsx
+++ app/explore/page.jsx
@@ -1,24 +1,53 @@
-import { View, Text, Dimensions } from "react-native";
-import React from "react";
+import { View, Text, Dimensions, Image } from "react-native";
+import React, { useContext } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
 import BackNav from "../components/BackNav";
 import MapView from "react-native-maps";
+import { UserLocationContext } from "../context/UserLocationContext";
+import { Marker } from "react-native-maps";
+import MapStyle from "./MapStyle.json";
 
 const page = () => {
   // get current route path
 
+  const { location, setLocation } = useContext(UserLocationContext);
   const screenHeight = Dimensions.get("window").height;
   return (
-    <SafeAreaView>
-      <StatusBar style="light" backgroundColor="#000" />
-      <View style={{ backgroundColor: "#000", height: screenHeight }}>
-        <BackNav path={"/"} />
-        <View>
-          <MapView style={{ width: 400, height: 300 }}></MapView>
+    location?.coords.latitude && (
+      <SafeAreaView>
+        <StatusBar style="light" backgroundColor="#000" />
+        <View style={{ backgroundColor: "#000", height: screenHeight }}>
+          <BackNav path={"/"} />
+          <View>
+          
+            <MapView
+              style={{ width: 400, height: 300 }}
+              customMapStyle={MapStyle}
+              region={{
+                latitude: location?.coords.latitude,
+                longitude: location?.coords.longitude,
+                latitudeDelta: 0.0222,
+                longitudeDelta: 0.0221,
+              }}
+            >
+              <Marker
+                coordinate={{
+                  latitude: location?.coords.latitude,
+                  longitude: location?.coords.longitude,
+                }}
+              >
+                <Image
+                  source={require("../../assets/images/marker.png")}
+                  style={{ width: 30, height: 30 }}
+                />
+              </Marker>
+            </MapView>
+            
+          </View>
         </View>
-      </View>
-    </SafeAreaView>
+      </SafeAreaView>
+    )
   );
 };
 
