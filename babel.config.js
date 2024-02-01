9941f598052353daef4714d6b766e1a0dee89140
Lazycoder26 <Rudra26_2004@outlook.com>
2024-02-01 12:23:43 +0530

    Update dependencies and configurations ( migrated from SDK 49 to 50 )

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      'expo-router/babel',
-      'module:react-native-dotenv'
-    ],
+    presets: ["babel-preset-expo"],
+    plugins: ["module:react-native-dotenv"],
   };
 };
